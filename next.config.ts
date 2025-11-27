632eb96fc03c997587e2eb4a0cc33e8631da59b0
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2025-11-27 12:00:48 +0000

    fix: remove unsupported i18n when export static

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,13 @@ const isProd = process.env.NODE_ENV === 'production';
 const repoBasePath = '/k402xxxcenxxx-blog';
 
 const nextConfig = {
-  output: 'export', // 讓 Next.js 可以 next export 成純靜態
+  output: 'export',
   images: {
-    unoptimized: true, // GitHub Pages 沒有 image optimizer，必須關掉
+    unoptimized: true,
   },
   basePath: isProd ? repoBasePath : '',
   assetPrefix: isProd ? repoBasePath : '',
-  trailingSlash: true, // 建議開啟，避免 GitHub Pages 對路徑的某些奇怪行為
-  i18n: {
-    locales: ['en', 'zh'],
-    defaultLocale: 'en',
-  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
